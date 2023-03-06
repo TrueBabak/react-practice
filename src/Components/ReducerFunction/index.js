1f40058a7a016d5a2fa31be2ae6c4545967dee91
Babak <jahanibabak80@gmail.com>
2023-03-06 04:54:51 +0330

    complicated action & dispatch in useReducer hook

--- src/Components/ReducerFunction/index.js
+++ src/Components/ReducerFunction/index.js
@@ -1,39 +1,46 @@
 import React, { useReducer } from "react";
+const resetState = 0;
+const initialState = {
+  Counter1: 0,
+  Counter2: 0,
+};
 const reducer = (state, action) => {
-  switch (action) {
+  switch (action.type) {
     case "addOne":
-      return state + 1;
-    case "addFive":
-      return state + 5;
+      return { Counter1: state.Counter1 + action.value };
+    case "Reset":
+      return { Counter1: resetState };
     case "decrement":
-      return state - 1;
+      return { Counter1: state.Counter1 - action.value };
 
     default:
-      return state;
+      return { Counter1: initialState };
   }
 };
 const ReducerFunc = () => {
-  const [Count, dispatch] = useReducer(reducer, 0);
+  const [Count, dispatch] = useReducer(reducer, initialState);
   return (
     <div className="w-full h-screen bg-[#0E8388] flex justify-center items-center">
       <div className="bg-[#2E4F4F] w-1/6 text-[#CBE4DE] m-auto py-4 rounded-xl px-4">
-        <h1 className="text-center text-3xl mb-4 font-mono">{Count}</h1>
+        <h1 className="text-center text-3xl mb-4 font-mono">
+          {Count.Counter1}
+        </h1>
         <div className="flex justify-between">
           <button
             className="border border-[#CBE4DE] px-2 py-1 rounded-xl hover:bg-[#2C3333] hover:text-white transition-all duration-300 hover:border-transparent select-none cursor-pointer"
-            onClick={() => dispatch("addOne")}
+            onClick={() => dispatch({ type: "addOne", value: 1 })}
           >
             Add one
           </button>
           <button
             className="border border-[#CBE4DE] px-2 py-1 rounded-xl hover:bg-[#2C3333] hover:text-white transition-all duration-300 hover:border-transparent select-none cursor-pointer"
-            onClick={() => dispatch("addFive")}
+            onClick={() => dispatch({ type: "Reset", value: resetState })}
           >
-            Add five
+            Reset
           </button>
           <button
             className="border border-[#CBE4DE] px-2 py-1 rounded-xl hover:bg-[#2C3333] hover:text-white transition-all duration-300 hover:border-transparent select-none cursor-pointer"
-            onClick={() => dispatch("decrement")}
+            onClick={() => dispatch({ type: "decrement", value: -1 })}
           >
             Min 1
           </button>
