559e47b8439481412645c86c3e6dcdb986c62bc2
Babak <jahanibabak80@gmail.com>
2023-03-06 04:18:20 +0330

    complite reducer function and design

--- src/Components/ReducerFunction/index.js
+++ src/Components/ReducerFunction/index.js
@@ -18,7 +18,27 @@ const ReducerFunc = () => {
   return (
     <div className="w-full h-screen bg-[#0E8388] flex justify-center items-center">
       <div className="bg-[#2E4F4F] w-1/6 text-[#CBE4DE] m-auto py-4 rounded-xl px-4">
-        reducer
+        <h1 className="text-center text-3xl mb-4 font-mono">{Count}</h1>
+        <div className="flex justify-between">
+          <button
+            className="border border-[#CBE4DE] px-2 py-1 rounded-xl hover:bg-[#2C3333] hover:text-white transition-all duration-300 hover:border-transparent select-none cursor-pointer"
+            onClick={() => dispatch("addOne")}
+          >
+            Add one
+          </button>
+          <button
+            className="border border-[#CBE4DE] px-2 py-1 rounded-xl hover:bg-[#2C3333] hover:text-white transition-all duration-300 hover:border-transparent select-none cursor-pointer"
+            onClick={() => dispatch("addFive")}
+          >
+            Add five
+          </button>
+          <button
+            className="border border-[#CBE4DE] px-2 py-1 rounded-xl hover:bg-[#2C3333] hover:text-white transition-all duration-300 hover:border-transparent select-none cursor-pointer"
+            onClick={() => dispatch("decrement")}
+          >
+            Min 1
+          </button>
+        </div>
       </div>
     </div>
   );
